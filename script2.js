6ddaabd07712c51f8f35cfbbed522c67396f8797
RICARDO FERNANDES DE MOURA <05380295010@senacrs.edu.br>
2025-03-28 19:14:23 -0300

    resoluções

---
+++ script2.js
@@ -0,0 +1,50 @@
+// console.log("rodou")
+
+// // VARIAVEIS
+// let nomedavariavel = ""
+// const nome = ""
+// // DADOS
+// "string" = texto 
+// number 
+// Array = ["a" , 0,90]
+// boolean = true ou false
+// isNaN = NaN
+// Object = {}
+// let obj = {
+//     name: "Ricardo"
+//     age: 35
+
+// }
+// obj.age
+
+// //FOR 
+// for(let i = 0; i<10; i++){
+//     console.log(i)
+//     let name23 = ""
+// }
+
+// //COMPARAÇÕES
+// if(condicao){
+
+// }else if(condicao2){
+
+// }else {
+
+// }
+// // && , ||, < , > , >= , <= , !=, ==, ===
+// //!isNan()
+// // tratament de valores
+// let numa = 0
+// numa.toFixed()
+// parseInt()
+// let name2 = ""
+// name2.toLowerCase() || name2.toUpperCase()
+// parseFloat()
+ // prompt()
+ // alert()
+
+// let lista = []
+// lista.push("a")
+
+ //FUNCTIONS
+
