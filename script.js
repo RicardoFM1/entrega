5bb7afc142f9004218b5730da17361e8f4224b83
RICARDO FERNANDES DE MOURA <05380295010@senacrs.edu.br>
2025-03-28 19:35:35 -0300

    resoluções

--- script.js
+++ script.js
@@ -159,10 +159,7 @@ imprimirinvertido()
 // retorne o seu dígito verificador.
 //  Para encontrar o dígito verificador, siga o exemplo do número 235:
 
-// Somar o número da conta com o seu inverso: 235 + 532 = 767; 
-// Multiplicar cada dígito pela sua ordem posicional (da esquerda para a direita) e 
-// somar estes resultados: 7 * 1 + 6 * 2 + 7 * 3 = 40; O último dígito desse resultado é o dígito verificador da conta:
-//  último dígito de 40 é 0.
+
 
 
 
